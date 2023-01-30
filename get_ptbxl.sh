7fefa7c408bea28038e16a7409316a9f10b49ba4
Zhangshuojackpot <49812574+Zhangshuojackpot@users.noreply.github.com>
2023-01-30 13:58:42 +0800

    Add files via upload

---
+++ get_ptbxl.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+####################################
+#   GET PTBXL DATABASE
+####################################
+mkdir -p data
+cd data
+wget https://storage.googleapis.com/ptb-xl-1.0.1.physionet.org/ptb-xl-a-large-publicly-available-electrocardiography-dataset-1.0.1.zip
+unzip ptb-xl-a-large-publicly-available-electrocardiography-dataset-1.0.1.zip
+mv ptb-xl-a-large-publicly-available-electrocardiography-dataset-1.0.1 ptbxl
+cd ..
+
